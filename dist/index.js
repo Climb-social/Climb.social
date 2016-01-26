0d297ea84467deeb227ac63f066e97b195761565
Michele Memoli <michele@100shapes.com>
2016-01-26 14:37:06 +0000

    chore(build): Added new build file
    Added build dist

--- dist/index.js
+++ dist/index.js
@@ -12,11 +12,29 @@ var _fetchJsonp = require('fetch-jsonp');
 
 var _fetchJsonp2 = _interopRequireDefault(_fetchJsonp);
 
+var _url = require('url');
+
 var getStream = function getStream(collectionId) {
+    var intervalSeconds = arguments.length <= 1 || arguments[1] === undefined ? 8 : arguments[1];
+    var domain = arguments.length <= 2 || arguments[2] === undefined ? 'http://app.climb.social' : arguments[2];
 
-    var pollRate = 5 * 1000;
+    if (!collectionId) {
+        throw new Error('Please specify a collectionId');
+    }
 
-    var requestStream = _rxLite.Observable.just('http://app.climb.social/api/v1/collections/' + collectionId);
+    if (typeof intervalSeconds !== 'number') {
+        throw new Error('Polling interval should be a positive integer. A ' + typeof intervalSeconds + ' was provided.');
+    }
+
+    if (intervalSeconds <= 0) {
+        throw new Error('Polling interval should be a positive integer. ' + intervalSeconds + ' was specified.');
+    }
+
+    var pollRate = intervalSeconds * 1000;
+
+    var URL = (0, _url.resolve)(domain, '/api/v1/collections/' + collectionId);
+
+    var requestStream = _rxLite.Observable.just(URL);
 
     var updateStream = _rxLite.Observable.interval(pollRate).startWith(null);
 
