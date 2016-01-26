13f705184a3328104e95d3d0bd458414a212512a
Michele Memoli <michele@100shapes.com>
2016-01-26 13:26:54 +0000

    feat(domain): Ability to set the domain that the library pings
    YOu can override the domain that this library pings for testing purposes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import {Observable} from 'rx-lite';
 import fetchJsonp from 'fetch-jsonp';
+import {resolve, parse} from 'url';
 
-const getStream = (collectionId, intervalSeconds = 8) => {
+const getStream = (
+  collectionId,
+  intervalSeconds = 8,
+  domain = 'http://app.climb.social'
+) => {
 
     if (!collectionId) {
         throw new Error('Please specify a collectionId');
@@ -17,7 +22,9 @@ const getStream = (collectionId, intervalSeconds = 8) => {
 
     const pollRate =  intervalSeconds * 1000;
 
-    const requestStream = Observable.just(`http://app.climb.social/api/v1/collections/${collectionId}`);
+    const URL = resolve(domain, `/api/v1/collections/${collectionId}`);
+
+    const requestStream = Observable.just(URL);
 
     const updateStream = Observable.interval(pollRate).startWith(null);
 
